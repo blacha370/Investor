81680c1fde84e7e3783edf9d0c0ceac3e49b2ca9
Łukasz Blacha <lukasz.blacha370@gmail.com>
2020-09-25 18:28:18 +0200

    created RequestHandler

---
+++ RequestHandler.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class RequestHandler:
+    @staticmethod
+    def send_request(symbol: str, api_key: str):
+        r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol
+                         + '&&apikey=' + api_key)
+        data = r.json()
+        values = list()
+        for day in list(data['Time Series (Daily)'].values())[0:30]:
+            day_open = float(day['1. open'])
+            day_close = float(day['4. close'])
+            day_change = round((day_close - day_open) / day_open, 5)
+            values.append((day_open, day_close, day_change))
+        return values
+
