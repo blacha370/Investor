b9e7d5ec7e7b14b7734f04e32ac7039430cfba9f
Łukasz Blacha <lukasz.blacha370@gmail.com>
2020-09-26 18:30:05 +0200

    created App class which controls other classes

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+from WebScraper import WebScraper
+from MarketStock import MarketStock
+import time
 
-def main():
-    print('OK')
 
+class App:
+    def __init__(self, path, api_key):
+        self.scraper = WebScraper(path)
+        self.today_active_stocks = list()
+        self.api_key = api_key
+        self.get_today_active_stocks()
 
-if __name__ == '__main__':
-    main()
+    def get_today_active_stocks(self):
+        for symbol in self.scraper.get_most_active_stocks(5):
+            self.today_active_stocks.append(MarketStock(symbol, self.api_key))
+            time.sleep(15)
