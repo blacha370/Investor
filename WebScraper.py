eee64c92060b1a883313557852a6f5951eb722c0
Łukasz Blacha <lukasz.blacha370@gmail.com>
2020-10-03 23:06:30 +0200

    add check_current_stock_value method, replace check_if_element_disappear with check if element appear

--- WebScraper.py
+++ WebScraper.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 class WebScraper:
@@ -12,7 +13,9 @@ class WebScraper:
         self.chromedriver_path = path
 
     def create_driver(self):
-        return webdriver.Chrome(executable_path=self.chromedriver_path, options=self.driver_option)
+        browser = webdriver.Chrome(executable_path=self.chromedriver_path, options=self.driver_option)
+        browser.minimize_window()
+        return browser
 
     @staticmethod
     def get_element(browser: webdriver.Chrome, path, timeout: int = 10):
@@ -21,10 +24,12 @@ class WebScraper:
         return browser.find_element_by_xpath(path)
 
     @staticmethod
-    def check_if_element_disappear(browser: webdriver.Chrome, path, timeout: int = 10):
+    def check_if_element_appear(browser: webdriver.Chrome, path, timeout: int = 10):
         try:
             WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.XPATH, path)))
         except TimeoutException:
+            return True
+        except StaleElementReferenceException:
             return True
         finally:
             return False
@@ -36,7 +41,7 @@ class WebScraper:
         cookie_btn = WebScraper.get_element(
             browser, '//div[@class="actions couple"]//form[@class="consent-form"]//button[@class="btn primary"]')
         cookie_btn.click()
-        while WebScraper.check_if_element_disappear(
+        while WebScraper.check_if_element_appear(
                 browser, '//div[@class="actions couple"]//form[@class="consent-form"]//button[@class="btn primary"]',
                 timeout=1):
             pass
@@ -44,7 +49,7 @@ class WebScraper:
             change_btn = WebScraper.get_element(
                 browser, '//div[@class="Ovx(a) Ovx(h)--print Ovy(h) "]//table[@class="W(100%)"]//thead//tr//th[5]')
             change_btn.click()
-            WebScraper.check_if_element_disappear(
+            WebScraper.check_if_element_appear(
                 browser, '//section[@class="Ta(start) Bxz(bb) Px(20px) Maw($newGridWidth) Bgc($lv2BgColor) Mx(a)]"//div[@class="Pos(a)"',
                 timeout=1)
 
@@ -55,3 +60,23 @@ class WebScraper:
             stocks_symbols.append(symbol.text)
         browser.quit()
         return stocks_symbols
+
+    def check_current_stock_value(self, symbol):
+        browser = self.create_driver()
+        browser.get('https://finance.yahoo.com/quote/' + symbol + '?p=' + symbol)
+        cookie_btn = WebScraper.get_element(
+            browser, '//div[@class="actions couple"]//form[@class="consent-form"]//button[@class="btn primary"]')
+        cookie_btn.click()
+        while WebScraper.check_if_element_appear(
+                browser, '//table[@class="W(100%)"]//tbody//tr[2]//td[2]//span',
+                timeout=1):
+            pass
+        open_value = WebScraper.get_element(browser, '//table[@class="W(100%)"]//tbody//tr[2]//td[2]//span').text
+        ask_value = WebScraper.get_element(browser, '//table[@class="W(100%)"]//tbody//tr[4]//td[2]//span').text
+        bid_value = WebScraper.get_element(browser, '//table[@class="W(100%)"]//tbody//tr[3]//td[2]//span').text
+        open_value = float(open_value)
+        ask_value = float(ask_value.split(' ')[0])
+        bid_value = float(bid_value.split(' ')[0])
+
+        browser.quit()
+        return open_value, ask_value, bid_value
